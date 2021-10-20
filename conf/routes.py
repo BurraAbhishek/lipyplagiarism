9920b1008419b3af536a14fd6f933409dd7654c2
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-10-20 15:59:12 +0530

    Fix render errors by adding parameters

--- conf/routes.py
+++ conf/routes.py
@@ -5,4 +5,9 @@ def PDFToText():
     print('Success')
     input_file = request.files['input_text_in_file']
     output_file = fromPDF.getTextFromPDF(input_file)
-    return render_template('index.html', inputtext=output_file)
+    return render_template(
+        'index.html', 
+        inputtext=output_file,
+        head="common/head.html", 
+        appbar="common/appbar.html"        
+        )
