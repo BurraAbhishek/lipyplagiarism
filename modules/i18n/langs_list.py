7bd2e7f5da4aaaead62279396f18fdf6233266c4
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-10-20 15:51:13 +0530

    Show languages list in settings UI

---
+++ modules/i18n/langs_list.py
@@ -0,0 +1,22 @@
+langs_list = [
+    {
+        "language": "English",
+        "langcode": "en",
+        "langsource": "translations/source/site.json"
+    },
+    {
+        "language": "Deutsch",
+        "langcode": "de-DE",
+        "langsource": "translations/dest/site/de-DE.json"
+    }, 
+    {
+        "language": "English (US)",
+        "langcode": "en-US",
+        "langsource": "translations/dest/site/en-US.json"
+    },
+    {
+        "language": "français",
+        "langcode": "fr-FR",
+        "langsource": "translations/dest/site/fr-FR.json"
+    },     
+]
