d13d8a80d2cf279bb86ca02ea687762cfbf76485
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-11-18 23:57:11 +0530

    Route for pre-processing data

--- conf/routes.py
+++ conf/routes.py
@@ -1,5 +1,7 @@
 from flask import request, render_template
 from modules.getText import fromPDF
+from modules.plagiarism.main import calculate_plagiarism
+
 
 def PDFToText():
     print('Success')
@@ -8,6 +10,16 @@ def PDFToText():
     return render_template(
         'index.html', 
         inputtext=output_file,
+        head="common/head.html", 
+        appbar="common/appbar.html"        
+        )
+
+
+def checkTextInitial(text):
+    print(calculate_plagiarism(text['input_text']))
+    return render_template(
+        'index.html', 
+        inputtext=text['input_text'],
         head="common/head.html", 
         appbar="common/appbar.html"        
         )
