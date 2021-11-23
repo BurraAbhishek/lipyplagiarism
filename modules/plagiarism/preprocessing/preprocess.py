cae4d5b0c39e4116cdf0789937d8aebfecf81c98
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-11-23 15:21:43 +0530

    preprocess text in separate module

---
+++ modules/plagiarism/preprocessing/preprocess.py
@@ -0,0 +1,15 @@
+from modules.plagiarism.preprocessing import vectorizedocument
+from modules.plagiarism.preprocessing import parasplitter
+from modules.plagiarism.preprocessing import removepunctuations
+from modules.plagiarism.preprocessing import removebullets
+
+def preprocess(text):
+    """ Preprocess a given text """
+    
+    s = vectorizedocument.text_to_list(text)
+    t = []
+    for i in s:
+        t.append(parasplitter.sbd(i))
+    t = removepunctuations.removesb(t)
+    t = removebullets.removebullets(t)
+    return t
