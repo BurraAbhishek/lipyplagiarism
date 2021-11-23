18a9972ebb997b58960f0fd91c638a407d1115fd
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-11-23 15:26:11 +0530

    Don't show preprocessing here
    Preprocessing is moved to its own module.

--- modules/plagiarism/main.py
+++ modules/plagiarism/main.py
@@ -1,20 +1,6 @@
-from modules.plagiarism.preprocessing import vectorizedocument
-from modules.plagiarism.preprocessing import parasplitter
-from modules.plagiarism.preprocessing import removepunctuations
-from modules.plagiarism.preprocessing import removebullets
-
-def preprocess(text):
-    """ Preprocess a given text """
-    
-    s = vectorizedocument.text_to_list(text)
-    t = []
-    for i in s:
-        t.append(parasplitter.sbd(i))
-    t = removepunctuations.removesb(t)
-    #t = removebullets.removebullets(t)
-    return t
+from modules.plagiarism.preprocessing import preprocess
 
 
 def calculate_plagiarism(text):
-    t = preprocess(text)
+    t = preprocess.preprocess(text)
     return t
