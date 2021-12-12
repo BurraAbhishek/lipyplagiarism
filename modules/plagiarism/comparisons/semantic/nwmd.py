e6bd29abee475043601c853d7f6beeb4759f7a3f
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-12-13 00:07:31 +0530

    init_sims is deprecated - use fill_norms instead.

--- modules/plagiarism/comparisons/semantic/nwmd.py
+++ modules/plagiarism/comparisons/semantic/nwmd.py
@@ -2,10 +2,19 @@
 
 from gensim import models
 
+# If this code fails to run, you need to download the GloVe dataset.
+# Open a file explorer, then go to modules/plagiarism/comparisons/datasets/
+# Create a new folder "glove6b" and go to the following link to download the dataset:
+# https://nlp.stanford.edu/data/wordvecs/glove.6B.zip
+# Then use the 50d model. If any other model is used, change the file name accordingly.
+# Before you can use the model, open a terminal or its equivalent in the same folder, then run
+# python -m gensim.scripts.glove2word2vec --input  glove.6B.50d.txt --output glove.6B.50d.w2vformat.txt
 w = models.KeyedVectors.load_word2vec_format(
     'modules/plagiarism/comparisons/datasets/glove6b/glove.6B.50d.w2vformat.txt', binary=False)
 
-w.init_sims(replace=True)
+# w.init_sims may throw a deprecation warning, use fill_norms instead.
+# w.init_sims(replace=True)
+w.fill_norms()
 
 def nwmd(dataframe):
     dataframe["Normalized Word Mover's Distance"] = dataframe.apply(
